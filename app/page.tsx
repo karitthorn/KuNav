9a5eb6f8b54bd92a4d129e553e5f9acf8c3fd3cb
karitthorn <56643494+karitthorn@users.noreply.github.com>
2023-10-19 19:22:34 +0700

    Fix deploy🚀

--- app/page.tsx
+++ app/page.tsx
@@ -63,9 +63,9 @@ export default function Home() {
                     >
                       <path
                         stroke="currentColor"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        stroke-width="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
                         d="m1 1 4 4 4-4"
                       />
                     </svg>
